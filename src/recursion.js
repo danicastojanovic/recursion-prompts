78f221e5a4dbe8e4ea43d50db381d84d1f15f180
danicastojanovic <dnc.stjnvc@gmail.com>
2021-01-01 11:44:22 -0800

    Completed compareStr

--- src/recursion.js
+++ src/recursion.js
@@ -269,6 +269,16 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+  // console.log(str1[0]);
+  if (str1[0] !== str2[0]) {
+    return false;
+  } else {
+    return compareStr(str1.slice(1), str2.slice(1));
+  }
+
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
